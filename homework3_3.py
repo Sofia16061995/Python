9c46094464c335e9bc8da2629c7c887c5ce3a5d7
Sofia <sofi.zoloto@yandex.ru>
2022-10-06 18:40:44 +0300

    add homework 3_3

---
+++ homework3_3.py
@@ -0,0 +1,14 @@
+import math
+numbers = list(input('Введите числа через запятую: ').split(','))
+min = math.inf
+max = 0
+for i in range(len(numbers)):
+    if ('.') in str(numbers[i]):        
+        if float(('0.'+ str(numbers[i]).split('.')[1])) > max:
+            max = float('0.'+str(numbers[i]).split('.')[1])
+        elif float('0.'+ str(numbers[i]).split('.')[1]) < min:
+            min = float('0.'+ str(numbers[i]).split('.')[1])
+print('Исходный список: ', numbers)
+print('Максмальное значение дробной части: ', max)
+print('Минимальное значение дробной части: ', min)
+print('Разница: ', max-min)
