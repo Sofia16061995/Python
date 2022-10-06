8649fec199fd90e870d2eb17b5f39a4ee2592ca1
Sofia <sofi.zoloto@yandex.ru>
2022-10-06 18:43:23 +0300

    add homework 3_4

---
+++ homework3_4.py
@@ -0,0 +1,9 @@
+def numberinbin(number):
+    if number == 0:
+        return []
+    else:
+        res = numberinbin(number//2)
+        res.append(number%2)
+        return res
+number = int(input('Введите число: '))
+print('Число в двоичной системе: ', "".join(map(str,numberinbin(number))))
