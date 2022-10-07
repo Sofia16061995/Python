ab81a298f2aa82ba6ee1c45bdaaa8851af2d2bfc
Sofia <sofi.zoloto@yandex.ru>
2022-10-07 23:09:05 +0300

    add homework 4_3

---
+++ 4seminar/homework4_3.py
@@ -0,0 +1,8 @@
+listNumbers = list(map(int, input("Введите числа через пробел: ").split()))
+
+print(f"Исходный список: {listNumbers}")
+
+tempListNumbers = []
+[tempListNumbers.append(i) for i in listNumbers if i not in tempListNumbers]
+
+print(f"Список из неповторяющихся элементов: {tempListNumbers}")
